396442cb0b60604e419788b9b7cbfc90992b7ee0
Nafisa <38379419+Nafisa-A-Ahmed@users.noreply.github.com>
2022-06-23 23:17:59 -0400

    Add files via upload

--- clustered_barchart.py
+++ clustered_barchart.py
@@ -12,27 +12,22 @@ def draw_barchart(data):
 
     )
 
-    # print(data.columns)
 
     colors = ['#630000', "#a80000",'#b8664f']  # ['#202020','#4f4f4f','#9e9e9e']  "
     years = ['2019', '2000', '1990']
+    
     for color, year in zip(colors, years):
         fig.add_trace(go.Bar(name=year, x=data[year], y=data['Region'], orientation='h', marker_color=color,
                              hovertemplate='<b>Region</b> : %{y}<br><b>Year</b> : ' + year + '<br><b>Mortality Rate</b> : %{x}<extra></extra>'
                              ))
-
-    # print(list(data['Region']))
-
-    fig.update_layout(barmode='group',
-                      yaxis=dict(categoryorder='array',
-                                 categoryarray=data['Region']),
-
+    
+    
+    fig.update_layout(barmode='group',   
                       legend={'traceorder': 'reversed'},
                       xaxis=dict(dtick=20, title="Mortality rate per 1000"),
                       title_text="Mortality Rate of Children under 5 Years Per Region",
                       height=700, width=800, margin_r=10, margin_b=20, margin_pad=4, margin_autoexpand=True
                       # yaxis = dict(autorange='reversed')
-
                       )
 
     return fig
