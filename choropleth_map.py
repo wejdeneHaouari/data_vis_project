8838e99575c403a0e67071736221dcd8625b0b28
Asmaih <45672358+asmaih@users.noreply.github.com>
2022-06-24 16:54:33 -0400

    add a border around the map

--- choropleth_map.py
+++ choropleth_map.py
@@ -19,19 +19,19 @@ def get_map(my_df, variable, VarList):
                         color=variable,
                         hover_name="Country",
                         color_continuous_scale=px.colors.sequential.Reds)
-    fig.update_layout(margin={"r": 40, "t": 40, "l": 40, "b": 40})
     fig.update_traces(hovertemplate=hover_template.get_map_template(),
                       customdata=my_df["Country"])
     fig.update_layout(title_text=variable_to_print, title_font_family="Arial",
                       title_font_size=24,
-                      geo=dict(showframe=False,
+                      geo=dict(showframe=True,
                                showcoastlines=False,
                                projection_type='equirectangular'))
+    fig.update_layout(margin={"r": 40, "t": 40, "l": 40, "b": 40})
     return fig
 
 
 def getName(variable, VarList):
     for i in VarList:
         if i["value"] == variable:
-            Vname = i["label"]  
+            Vname = i["label"]
     return Vname
