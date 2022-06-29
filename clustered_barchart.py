5554e1e25709cf4bd7a2bd0c706cca46c54e9d11
Nafisa <38379419+Nafisa-A-Ahmed@users.noreply.github.com>
2022-06-29 05:21:39 -0400

    added comments, call func from hover template

--- clustered_barchart.py
+++ clustered_barchart.py
@@ -1,30 +1,37 @@
+'''
+This file contains the draw_clusteredBarchart() that plot the mortality rate of children under 5 per refion
+For each region the mortality rate is shown for three years (1990,2000,2019)
+'''
+
 import plotly.graph_objects as go
 import plotly.io as pio
+import hover_template
 
-
-def draw_barchart(data):
+def draw_clusteredBarchart(data):
     fig = go.Figure()
+    
     fig.update_layout(
         template=pio.templates['simple_white'],
         dragmode=False,
-        barmode='relative',
-        # title="Mortality Rate of Children Under 5 Years"
-
+        #barmode='relative'
     )
-    colors = ['#630000', "#a80000", '#b8664f']  
+  
+    colors = ['#630000', "#a80000", '#b8664f']    
     years = ['2019', '2000', '1990']
     for color, year in zip(colors, years):
         fig.add_trace(go.Bar(name=year, x=data[year], y=data['Region'], 
                              orientation='h', marker_color=color,
-                             hovertemplate='<b>Region</b> : %{y}<br><b>Year</b> : ' + year + '<br><b>Mortality Rate</b> : %{x}<extra></extra>'
-                             ))
+                             hovertemplate=hover_template.get_clustBarChart_template(year)))
+        
     fig.update_layout(barmode='group',
                       legend={'traceorder': 'reversed'},
-                      xaxis=dict(dtick=20, title="Mortality rate per 1000"),
+                      xaxis={'dtick':20, 'title':'Mortality rate per 1000',
+                             'title_font_family':'sans-serf'},
+                      yaxis={'title_font_family':'sans-serf'},
                       title_text="Mortality Rate of Children under 5 Years Per Region",
+                      title_font_family='sans-serf',
                       height=700, width=800, margin_r=10, margin_b=20,
                       margin_pad=4, margin_autoexpand=True
-                      # yaxis = dict(autorange='reversed')
                       )
 
     return fig
